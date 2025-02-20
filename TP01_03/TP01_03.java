97eb66f0e87c7c301e4395cc04699917f0f2184f
Samuelvieria <samuelalvesvieira39@gmail.com>
2025-02-20 16:58:03 -0300

    q3

--- TP01_03/TP01_03.java
+++ TP01_03/TP01_03.java
@@ -1,14 +1,32 @@
-import java.util.*;
-
-
+package TP01_03;
+import java.util.Scanner;
 
 public class TP01_03 {
 
-    public static void main(String[] args) {
+    public static String ciframento(String palavra) {
+        int tamanho = palavra.length();
+        char[] palavraChar = palavra.toCharArray();
 
-        System.out.println(a+3);
-        
+        for (int i = 0; i < tamanho; i++) {
+            palavraChar[i] = (char) (palavraChar[i] + 3);
+        }
+
+        return new String(palavraChar);
     }
 
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("FIM")) {
+                break;
+            }
+
+           System.out.println(ciframento(input));
+        }
+
+        scanner.close();
+    }
 }
