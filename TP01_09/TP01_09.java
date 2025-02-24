f9bbf30eb3ffb2c1617023076fcb307a84e103a1
Samuelvieria <samuelalvesvieira39@gmail.com>
2025-02-24 17:42:49 -0300

    estáerrado

--- TP01_09/TP01_09.java
+++ TP01_09/TP01_09.java
@@ -1,50 +1,42 @@
-package TP01_09;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class TP01_09 {
 
-    public static void Anagrama(String p1, String p2){
-
+    public static void Anagrama(String p1, String p2) {
         char[] p1Char = p1.toCharArray();
         char[] p2Char = p2.toCharArray();
-    
+
         Arrays.sort(p1Char);
         Arrays.sort(p2Char);
-    
-        
+
         String a = new String(p1Char);
         String b = new String(p2Char);
-    
-        if(a.equals(b)){
+
+        if (a.equals(b)) {
             System.out.println("SIM");
-        }else{
+        } else {
             System.out.println("NÃO");
         }
-    
-       
-    
-    
     }
-    
 
     public static void main(String[] args) {
         Scanner palavra = new Scanner(System.in);
 
-        while (true) {
+        while (palavra.hasNextLine()) {
             String palavra2 = palavra.nextLine();
-            String palavra3 = palavra.nextLine();
             if (palavra2.equals("FIM")) {
-                palavra.close();
-                return;
+                break;  
             }
 
-            Anagrama(palavra2,palavra3);
+            if (palavra.hasNextLine()) {
+                String palavra3 = palavra.nextLine();
+                Anagrama(palavra2, palavra3);
+            } else {
+                break;  
+            }
         }
-        
-    }
 
-    
+        palavra.close();  
+    }
 }
